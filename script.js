8714a8e60cc8ebbea2b61dd897906bf79774edfb
Rares Herta <rares.herta@endava.com>
2020-03-28 12:22:35 +0200

    Video progress bar and timestamp

--- script.js
+++ script.js
@@ -20,12 +20,27 @@ function updatePlayIcon() {
 
 // Update progress & timestamp
 function updateProgress() {
-	return true;
+	// Get the percentage of the video for the progress bar
+	progress.value = (video.currentTime / video.duration) * 100;
+
+	// Get minutes
+	let mins = Math.floor(video.currentTime / 60);
+	if (mins < 10) {
+		mins = '0' + String(mins);
+	}
+
+	// Get seconds
+	let secs = Math.floor(video.currentTime % 60);
+	if (secs < 10) {
+		secs = '0' + String(secs);
+	}
+
+	timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-	return true;
+	video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
